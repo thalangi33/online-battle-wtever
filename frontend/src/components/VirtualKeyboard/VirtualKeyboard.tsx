ac08ead4f2895e8179d72be32099cd846fcf41ed
ithalang0 <79791061+ithalang0@users.noreply.github.com>
2022-07-10 20:39:46 +0800

    Add functions and stylings to Word_UI

---
+++ frontend/src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VirtualKeyboard = () => {
+  return (
+    <div>VirtualKeyboard</div>
+  )
+}
+
+export default VirtualKeyboard
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Word_UI from '../components/Word_UI/Word_UI'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Word_UI />
+    </div>
+  )
+}
+
+export default Home
