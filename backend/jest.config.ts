57f96250e18b33cce7e5622b3f6554660637f031
dannysze <dann.nn49825@gmail.com>
2022-08-12 22:39:25 +0800

    Update config

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,14 +1,19 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
   coverageDirectory: "coverage",
-  moduleDirectories: ["node_modules", "src"],
+  // moduleDirectories: ["node_modules", "src"],
+  modulePaths: ["src"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+    "@src/(.*)": "<rootDir>/src/$1",
   },
   preset: "ts-jest",
   testEnvironment: "node",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(mongodb)/)"],
   setupFilesAfterEnv: ["<rootDir>/src/matchers/index.ts"],
 };
+
+export default config;
